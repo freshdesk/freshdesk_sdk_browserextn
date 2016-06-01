a17e9f9df0eb1c579a16b2c5968cff7ab3cb3cf2
Swathi <swathit@freshdesk.com>
2016-06-01 15:37:58 +0530

    Design for Freshdesk developer chrome extension popup https://github.com/freshdesk/freshapps_devportal/issues/911

--- chrome/src/js/extension/popup.js
+++ chrome/src/js/extension/popup.js
@@ -4,7 +4,7 @@ var browserExtnVer = chrome.runtime.getManifest().version;
 var sdkVerPH = document.getElementById('sdk-version');
 var sdkCompatMsgPH = document.getElementById('sdk-compat-msg');
 
-browserExtnVerPH.innerHTML = browserExtnVer;
+browserExtnVerPH.innerHTML = 'Extn. Version: ' + browserExtnVer;
 
 function getSdkVersion() {
   jQuery.ajax(
@@ -15,12 +15,14 @@ function getSdkVersion() {
   )
   .done(
     function(data) {
-      sdkVerPH.innerHTML = data['sdk-version'];
+      jQuery('#sdk-version').addClass('sdk-version');
+      sdkVerPH.innerHTML = 'SDK Version: ' + data['sdk-version'];
     }
   )
   .fail(
     function() {
-      sdkVerPH.innerHTML = 'undef';
+      jQuery('#sdk-version').removeClass('sdk-version');
+      sdkVerPH.innerHTML = '';
     }
   );
 }
@@ -33,21 +35,24 @@ function sdkVersionCompatCheck() {
   )
   .done(
     function(data) {
-      sdkCompatMsgPH.innerHTML =  "<div class='success-wrap'><img src='img/tick.png'></div>"+
+      sdkCompatMsgPH.innerHTML =  "<div class='success-wrap'><img src='img/smiley-happy.svg' class='svg'></div>"+
                                   "<div class='status'>All Good! Happy Coding!</div>";
+      chrome.browserAction.setBadgeText({text: ''});
+      jQuery('.validation-msg').addClass('success');
     }
   )
   .fail(
     function(jqXHR, textStatus) {
       var msg, link;
       if(jqXHR.readyState == 4) {
-        msg = "<div class='status'>SDK is not compatible! Your SDK version: 2.3. Latest Version: 3.4 <br> Please update your SDK to use with this extension.</div>";
+        msg = "<div class='status'>SDK is not compatible. <br> Please update your SDK to use with this extension.</div>";
       }
       else if(jqXHR.readyState == 0) {
-        msg = "<div class='status'>Please start your SDK Server.</div>";
+        msg = "<div class='status'>Please start your SDK server.</div>";
       }
-      sdkCompatMsgPH.innerHTML = "<div class='error-wrap'><img src='img/error.png'></div>"+
-                                  msg;
+      sdkCompatMsgPH.innerHTML = "<div class='error-wrap'><img src='img/notice-o.svg' class='svg'></div>"+msg;
+      chrome.browserAction.setBadgeText({text: 'X'});
+      jQuery('.validation-msg').addClass('error');
     }
   );
 }
